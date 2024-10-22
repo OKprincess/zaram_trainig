c6a8a4882de1c492b26b97c7b12384a6cbe392b2
OKprincess <fiona0606@sookmyung.ac.kr>
2024-10-22 20:47:29 +0900

    comment

--- RV32I/core/common/riscv_configs.v
+++ RV32I/core/common/riscv_configs.v
@@ -11,12 +11,12 @@
 // ==================================================
 `define	DEBUG
 `define	XLEN			32
-`define	DMEM_ADDR_BIT	32
-`define	IMEM_ADDR_BIT	32	
+`define	DMEM_ADDR_BIT	16
+`define	IMEM_ADDR_BIT	16	
 
-//`define	DMEM_INIT
+`define	DMEM_INIT
 //`define	DMEM_INIT_FILE	"riscv_dmem.mif"
-//`define	IMEM_INIT
+`define	IMEM_INIT
 //`define	IMEM_INIT_FILE	"riscv_imem.mif"
 // ==================================================
 // 	ALU (Arithmetic & Logical Unit)	
@@ -56,8 +56,8 @@
 `define	SRC_IMM_I		3'b001
 `define	SRC_IMM_S		3'b010
 `define	SRC_IMM_B		3'b011
-`define	SRC_IMM_U		3'b100
-`define	SRC_IMM_J		3'b101
+`define	SRC_IMM_J		3'b100
+`define	SRC_IMM_U		3'b101
 `define SRC_IMM_X		3'b111
 
 // ==================================================
