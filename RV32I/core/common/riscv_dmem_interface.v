baaaa6ba9cdc87a7d512c9f0c6721599d0821fad
OKprincess <fiona0606@sookmyung.ac.kr>
2024-10-26 19:12:20 +0900

    comment

--- RV32I/core/common/riscv_dmem_interface.v
+++ RV32I/core/common/riscv_dmem_interface.v
@@ -35,28 +35,9 @@ module riscv_dmem_interface
 	// Bypass
 	assign		o_dmem_intf_addr	= i_dmem_intf_addr;
 	assign		o_dmem_intf_wen		= i_dmem_intf_wen;
-
-	//	To Data Memory
-	assign		o_dmem_intf_wr_data	= i_dmem_intf_wr_data	<< (8*i_dmem_intf_addr[1:0]);
-	assign		o_dmem_intf_byte_sel= i_dmem_intf_byte_sel 	<< (1*i_dmem_intf_addr[1:0]);
-
-	// To CPU
-	wire	[`XLEN-1:0] byte_aligned_dmem_rd_data;
-	assign	byte_aligned_dmem_rd_data = i_dmem_intf_rd_data >> (8*i_dmem_intf_addr[1:0]);
-
-	always @(*) begin
-		case (i_dmem_intf_func3)
-			`FUNCT3_MEM_BYTE	: o_dmem_intf_rd_data = {{24{byte_aligned_dmem_rd_data[ 7]}}, byte_aligned_dmem_rd_data[ 7:0]};
-			`FUNCT3_MEM_HALF	: o_dmem_intf_rd_data = {{16{byte_aligned_dmem_rd_data[15]}}, byte_aligned_dmem_rd_data[15:0]};
-			`FUNCT3_MEM_WORD	: o_dmem_intf_rd_data = {byte_aligned_dmem_rd_data[31:0]};
-			`FUNCT3_MEM_BYTEU	: o_dmem_intf_rd_data = {{24{1'b0}}, byte_aligned_dmem_rd_data[ 7:0]};
-			`FUNCT3_MEM_HALFU	: o_dmem_intf_rd_data = {{16{1'b0}}, byte_aligned_dmem_rd_data[15:0]};
-		endcase
-	end
-	
 	/* Strobe Signal catch read or write Signal
 	* if using this DQS in dmem_intf, input byte sel signal needX
-	* or Ctrl module.
+	* or Ctrl module.*/
 	reg			[3:0]	dmem_intf_strb;
 	always @(*) begin
 		case (i_dmem_intf_func3)
@@ -70,6 +51,26 @@ module riscv_dmem_interface
 			default				: dmem_intf_strb	= 4'b1111;
 		endcase
 	end
-	*/
+	
+
+	//	To Data Memory
+	assign		o_dmem_intf_wr_data	= i_dmem_intf_wr_data	<< (8*i_dmem_intf_addr[1:0]);
+	assign		o_dmem_intf_byte_sel= dmem_intf_strb	 	<< (1*i_dmem_intf_addr[1:0]);
+
+	// To CPU
+	wire	[`XLEN-1:0] byte_aligned_dmem_rd_data;
+	assign	byte_aligned_dmem_rd_data = i_dmem_intf_rd_data >> (8*i_dmem_intf_addr[1:0]);
+
+	always @(*) begin
+		case (i_dmem_intf_func3)
+			`FUNCT3_MEM_BYTE	: o_dmem_intf_rd_data = {{24{byte_aligned_dmem_rd_data[ 7]}}, byte_aligned_dmem_rd_data[ 7:0]};
+			`FUNCT3_MEM_HALF	: o_dmem_intf_rd_data = {{16{byte_aligned_dmem_rd_data[15]}}, byte_aligned_dmem_rd_data[15:0]};
+			`FUNCT3_MEM_WORD	: o_dmem_intf_rd_data = {byte_aligned_dmem_rd_data[31:0]};
+			`FUNCT3_MEM_BYTEU	: o_dmem_intf_rd_data = {{24{1'b0}}, byte_aligned_dmem_rd_data[ 7:0]};
+			`FUNCT3_MEM_HALFU	: o_dmem_intf_rd_data = {{16{1'b0}}, byte_aligned_dmem_rd_data[15:0]};
+			default				: o_dmem_intf_rd_data = 32'dx;
+		endcase
+	end
+	
 	
 endmodule
