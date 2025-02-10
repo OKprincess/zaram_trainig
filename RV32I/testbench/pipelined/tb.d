343f3005391f20d005ffecb800e16ae85e15be85
OKprincess <fiona0606@naver.com>
2025-02-10 13:53:07 +0900

    comment

--- RV32I/testbench/pipelined/tb.d
+++
@@ -1,7 +0,0 @@
-tb.o: ../tb.cpp /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Vriscv_top.h \
- /usr/share/verilator/include/verilated_heavy.h Vriscv_top__Dpi.h \
- /usr/share/verilator/include/vltstd/svdpi.h
