5496287f842a13b7214af985e990ec353fb2500e
OKprincess <fiona0606@naver.com>
2025-02-20 19:10:39 +0900

    comment

---
+++ Click_based/src/click_network.v
@@ -0,0 +1,16 @@
+// ===================================================
+//	=================[ VLSISYS Lab. ]=================
+//	* Author		: oksj (oksj@sookmyung.ac.kr)
+//	* Filename		: click_network.v
+//	* Description	: 
+// ===================================================
+`include		"dff.v"
+`include		"click.v"
+
+module click_network 
+(
+	output		o_fire_IF,o_fire_ID, o_fire_EX, o_fire_MEM, o_fire_WB,
+	output		
+);
+	
+endmodule
---
+++ Click_based/src/dff.v
@@ -0,0 +1,27 @@
+// ===================================================
+//	=================[ VLSISYS Lab. ]=================
+//	* Author		: oksj (oksj@sookmyung.ac.kr)
+//	* Filename		: dff.v
+//	* Description	: 
+// ===================================================
+ module	dff
+ #(
+	 parameter	BW_DATA	= 1
+ )
+ (
+	 output	reg	[BW_DATA-1:0]	o_q,
+	 input		[BW_DATA-1:0]	i_d,
+	 input						i_clk,
+	 input						i_rstn
+ );
+
+ 	always @(posedge i_clk or negedge i_rstn) begin
+		if(!i_rstn) begin
+			o_q	<= 1'b0;
+		end else begin
+			o_q	<=	i_d;
+		end
+ 	end
+
+endmodule
+
